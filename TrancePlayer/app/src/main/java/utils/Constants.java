d53d3fe121ca157e230fb4829ad9138b41c091de
Dash <dash@enclave.vn>
2015-09-30 16:06:18 +0700

    Implement all functionalities

---
+++ TrancePlayer/app/src/main/java/utils/Constants.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Created by duc.t.pham on 9/23/15.
+ */
+public class Constants {
+    public static final String SONGS_URL = "https://jsonblob.com/api/jsonBlob/560361f6e4b01190df3965ee";
+    public static final String KEY_ID = "id";
+    public static final String KEY_URL = "url";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_DURATION = "duration";
+    public static final String KEY_TRACK = "track";
+    public static final String KEY_TIME = "time";
+    public static final String KEY_NAME = "name";
+}
